4420002423b3c189f3e54fd7345c0fb11178995b
APolubarev <polubarev@gmail.com>
2023-02-10 00:56:32 +0300

    добавил проверку на уже есть файл с таким именем

--- FileRenamer.py
+++ FileRenamer.py
@@ -2,6 +2,20 @@ import os
 import subprocess
 import sys
 
+def get_valid_filename(directory, filename, extention, index):
+    if  index == 0:
+        strIndex = ""
+    else:
+        strIndex = " (" + str(index) + ")"
+
+    newFilename = filename + strIndex + extention
+
+    if os.path.exists(os.path.join(directory, newFilename)):
+        newFilename = get_valid_filename(directory, filename, extention, index + 1)
+
+    return newFilename
+
+
 if __name__ == '__main__':
 
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pymediainfo'])
@@ -10,7 +24,8 @@ if __name__ == '__main__':
     files = os.listdir(directory)
 
     for filename in files:
-        if filename.endswith('.MP4') or filename.endswith('.mp4') and filename.startswith('GH'):
+        alreadyDone = filename[:8].isnumeric() and filename[8] == "_" and filename[9:15].isnumeric();
+        if (filename.endswith('.MP4') or filename.endswith('.mp4')) and not alreadyDone:
             media_info = MediaInfo.parse(os.path.join(directory, filename))
             for track in media_info.tracks:
                 if track.track_type == "Video":
@@ -19,8 +34,9 @@ if __name__ == '__main__':
                     date_of = date_of.replace('UTC ', '')
                     date_of = date_of.replace('-', '')
                     date_of = date_of.replace(' ', '_')
+                    # newFilename = get_  date_of + filename[-4:];
 
-            os.rename(filename, date_of + filename[8:])
+                    os.rename(filename, get_valid_filename(directory, date_of, filename[-4:], 0))
 
 
 
